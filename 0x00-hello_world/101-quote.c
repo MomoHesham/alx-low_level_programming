81d115a63db7d90a4c3b3355d29c5e44c4cdf276
MomoHesham <mernah2012@gmail.com>
2023-04-24 05:35:05 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'Printing Function'
+ *
+ * Return: 1
+ *
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
+	return (1);
+}
