ed4a7ae1a881d3348be94592be573508d748edae
MomoHesham <mernah2012@gmail.com>
2023-04-24 17:59:56 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description:'printing numbers'
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
