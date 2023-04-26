e840ce9f9f931f51480100ca43ae9727b28cf836
MomoHesham <mernah2012@gmail.com>
2023-04-26 09:34:08 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,33 @@
 
 void times_table(void)
 {
-	int a, b, c;
+	int a, b, c, x, d;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			_putchar(c - '0');
-			_putchar(44);
-			_putchar(' ');
+			if (c > 9)
+			{
+				x = c % 10;
+				d = (c - x) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(x + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(c + '0');
+			}
 		}
 		_putchar('\n');
 	}
