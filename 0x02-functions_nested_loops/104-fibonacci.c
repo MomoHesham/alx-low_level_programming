9088daeee2ffb65d989ec552e0bc222ad1a1f837
MomoHesham <mernah2012@gmail.com>
2023-04-26 12:54:54 -0700

    98 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int i = 0;
+	long a1 = 1, a2 = 2;
+
+	while (i < 98)
+	{
+		if (i == 0)
+		{
+			printf("%ld", a1);
+		}
+		else if (i == 1)
+		{
+			printf(" ,%ld", a2);
+		}
+		else
+		{
+			a2 += a1;
+			a1 = a2 - a1;
+			printf(" ,%ld", a2);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
