67e622fdb18383ab47bfc1144576bcdd3d0b06b5
MomoHesham <mernah2012@gmail.com>
2023-04-27 02:59:30 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,40 +7,42 @@ void print_times_table(int n)
 {
 	int x = 0, y, z;
 
-	if (n > 15 || n < 0)
-		return;
-	while (x <= n)
+	if (n >= 0 && n <= 15)
 	{
 		for (y = 0; y <= n; y++)
 		{
-			z = x * y;
-			if (z > 99)
+			for (z = 0; z <= n; z++)
 			{
-				_putchar(z / 100 + '0');
-				_putchar((z / 10 % 10) + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (z > 9)
-			{
-				_putchar(' ');
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-			else
-				_putchar(z + '0');
-			if (y != n)
-			{
-				_putchar(',');
-				_putchar(' ');
+				x = y * z;
+				if (x > 99)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar((x / 100) + '0');
+					_putchar(((x / 10) % 10) + '0');
+					_putchar((x % 10) + '0');
+				}
+				else if (x > 9)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
+				}
+				else
+				{
+					if (z != 0)
+					{
+						_putchar(44);
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(x + '0');
+				}
 			}
 			_putchar('\n');
-			x++;
 		}
 	}
 }
