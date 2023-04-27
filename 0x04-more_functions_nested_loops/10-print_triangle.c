0c4e7fe7cac9b93c7adf259cb0886ab91774bdd3
MomoHesham <mernah2012@gmail.com>
2023-04-27 15:15:30 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 		{
 			for (i = size - h; i > 0; i++)
 				_putchar(' ');
-			for (i = 0; i < size; i++)
+			for (i = 0; i < h; i++)
 				_putchar('#');
 			if (h == size)
 				continue;
