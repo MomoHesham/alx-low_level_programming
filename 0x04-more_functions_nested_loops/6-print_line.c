3ab739a18d326deeffcef1e0c0c5064cafc96963
MomoHesham <mernah2012@gmail.com>
2023-04-27 07:27:59 -0700

    draw striaght

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,12 @@ void print_line(int n)
 {
 	int i;
 
-if (n > 0)
-{
-	for (i = 0; i <= n; i++)
+	if (n > 0)
 	{
-		_putchar('_');
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
 	}
-}
-_putchar('\n');
 }
