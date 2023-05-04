165379735bcf345f8961f93f521c1b6127e68790
MomoHesham <mernah2012@gmail.com>
2023-05-04 12:40:31 -0700

    copmare two string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,30 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, flag;
-
-	i = 0;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
+		;
+	if (*s1 == *s2)
 	{
-		if (s1[i] == s2[i])
-		{
-			flag = 0;
-		}
-		else if (s1[i] > s2[i])
-		{
-			flag = 1;
-		}
-		else
-		{
-			flag = -1;
-		}
-		i++;
-	}
-	if (flag == 0)
 		return (0);
-	else if (flag == -1)
-		return (-1);
-	else
-		return (1);
+	}
+	return (*s1 - *s2);
 }
