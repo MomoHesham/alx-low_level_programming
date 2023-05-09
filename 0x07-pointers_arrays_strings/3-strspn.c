aac69ed2e9b9535be3107df4db988e850cf6a70d
MomoHesham <mernah2012@gmail.com>
2023-05-09 16:02:33 +0300

    locates

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,13 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, count = 0;
+	int i, j, count = 0;
 
-	for (i = 0; i < strlen(s); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (count == strlen(accept))
-			break;
 		if (s[i] != 32)
 		{
-			for (j = 0; j < strlen(accept); j++)
+			for (j = 0; accept[j] != '\0'; j++)
 			{
 				if (s[i] == accept[j])
 					count++;
