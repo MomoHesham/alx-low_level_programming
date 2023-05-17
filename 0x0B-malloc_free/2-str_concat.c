286d737beb529f5e89ef3ee89bf033527d3e279d
MomoHesham <mernah2012@gmail.com>
2023-05-17 04:51:53 -0700

    concatenates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	res = malloc(sizeof(char) * (strlen(s1) + strlen(s2)));
+	res = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	if (res == NULL)
 		return (NULL);
 
