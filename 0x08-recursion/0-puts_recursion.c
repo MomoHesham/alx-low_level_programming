159ab1e1e0aa03fba3298ece07b1e97444e6f49f
MomoHesham <mernah2012@gmail.com>
2023-05-20 15:30:56 +0300

    readme

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints strings, followed by new line
+ * @s: array of string
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (!*s)
+	{
+		return;
+	}
+	write(1, s, 1);
+	_puts_recursion(s + 1);
+	_putchar('\n');
+}
