80c04a40650fc77e22f70f14e3cf9c237a151a93
MomoHesham <mernah2012@gmail.com>
2023-05-23 05:50:57 -0700

    creates new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,13 +13,38 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+	int nlen, olen, i;
 
-	newdog = malloc(sizeof(struct dog));
+
+	nlen = olen = 0;
+	while (name[nlen++])
+		;
+	while (owner[olen++])
+		;
+
+
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
+		return (NULL);
+
+	newdog->name = malloc(nlen * sizeof(newdog->name));
+	if (newdog == NULL)
+		return (NULL);
+
+	for (i = 0; i < nlen; i++)
+	{
+		newdog->name[i] = name[i];
+	}
+
+	newdog->age = age;
+
+	newdog->owner = malloc(olen * sizeof(newdog->owner));
 
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = name;
-	newdog->age = age;
-	newdog->owner = owner;
+
+	for (i = 0; i < olen; i++)
+		newdog->owner[i] = owner[i];
+
 	return (newdog);
 }
